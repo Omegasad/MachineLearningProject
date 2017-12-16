5d41f9176096851dd625a8d1f0ef8c0c918bf767
Name <alexu.wong@mail.utoronto.ca>
2017-12-16 14:45:37 -0500

    test

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from sklearn import tree
-#features as the input
+# features as the input
 features = [[140, 0], [130, 0], [150, 1], [170,1]]
 
-#labels as the output
+# labels as the output
 labels = [0, 0 , 1 , 1]
 
 # clf = classifier
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 
-#prints must have params
+# prints must have params
 print (clf.predict([[150,0]]))
+# wtf!!!!!
