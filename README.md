9cf7c11aa790415476e9618f3a055de1bfa703de
Omegasad <tkahoso@my.bcit.ca>
2017-12-19 10:48:37 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# MachineLearningProject
+
+This runs on Pycharm with Python version 3.6.3
--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-"""
-Docstring
-"""
-from sklearn import tree
-# features as the input
-features = [[140, 0], [130, 0], [150, 1], [170, 1]]
+# Imports
+# To use tensorflow Libraries
+import tensorflow as tf
 
-# labels as the output
-labels = [0, 0, 1, 1]
+# To manipulate datas
+import numpy as np
+import pandas as pd
 
-# clf = classifier
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
+# data = pd.read_csv("BTCtoUSD.csv",header=0,usecols=['Date','Price'],index_col=['Price'])
+# Read CSV
+#data = pd.read_csv("./csv/BTCtoUSD.csv")
+data = pd.read_csv("./csv/ETHtoUSD.csv")
+#data = pd.read_csv("./csv/LTCtoUSD.csv")
 
-# prints must have params
-print(clf.predict([[150, 0]]))
-# wtf!!!!!!!
-# Post thighs!!!!
+# Drop data
+data = data.drop(['Date','Change %'],1)
+#print(data.head(1795))
+
+# Data.shape[0] = Columns
+# Data.shape[1] = Rows
+n = data.shape[0] - 3
+p = data.shape[1]
+
+# Reverse order of chart so it goes lower to highest
+reversed_df = data.iloc[n::-1]
+#print(reversed_df)
+
+#Make Data to numpy array
+data = reversed_df.values
+print(data)
+
+# Use a regression with the data
+# Open : the price at the start of the day
+# Price : The price at the end of the day
+# High : Highest price at the given day
+# Low : Lowest price at the given day
