7f59a0bd8c740793beda5935172dd4c49a5feac5
Omegasad <tkahoso@my.bcit.ca>
2017-12-18 21:52:04 -0800

    Add files via upload
    Adding files for the project

---
+++ main.py
@@ -0,0 +1,32 @@
+# Imports
+# To use tensorflow Libraries
+import tensorflow as tf
+
+# To manipulate datas
+import numpy as np
+import pandas as pd
+
+# data = pd.read_csv("BTCtoUSD.csv",header=0,usecols=['Date','Price'],index_col=['Price'])
+# Read CSV
+#data = pd.read_csv("./csv/BTCtoUSD.csv")
+data = pd.read_csv("./csv/ETHtoUSD.csv")
+#data = pd.read_csv("./csv/LTCtoUSD.csv")
+
+# Drop data
+data = data.drop(['Date','Change %'],1)
+#print(data.head(1795))
+
+# Data.shape[0] = Columns
+# Data.shape[1] = Rows
+n = data.shape[0] - 3
+p = data.shape[1]
+
+# Reverse order of chart so it goes lower to highest
+reversed_df = data.iloc[n::-1]
+print(reversed_df)
+
+# Use a regression with the data
+# Open : the price at the start of the day
+# Price : The price at the end of the day
+# High : Highest price at the given day
+# Low : Lowest price at the given day
